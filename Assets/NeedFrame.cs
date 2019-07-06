c3296eedeeb83d0538b9dd74c09f9e5ed6519c1c
Fsly <2645358281@qq.com>
2019-07-06 14:46:04 +0800

    统一版本

---
+++ Assets/NeedFrame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeedFrame : MonoBehaviour
+{
+    public Transform playerTransform;
+
+    //相机跟随
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //相机跟随
+        offset = transform.position - playerTransform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //相机跟随
+        transform.position = playerTransform.position + offset;
+    }
+}
