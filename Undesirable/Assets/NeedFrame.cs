e11862958d3e643ce89d7db6f1cef51d96da55a6
Fsly <2645358281@qq.com>
2019-07-06 13:12:00 +0800

    捡东西

--- Undesirable/Assets/NeedFrame.cs
+++ Undesirable/Assets/NeedFrame.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class NeedFrame : MonoBehaviour
 {
+    public Transform playerTransform;
+
+    //相机跟随
+    private Vector3 offset;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //相机跟随
+        offset = transform.position - playerTransform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //相机跟随
+        transform.position = playerTransform.position + offset;
     }
 }
---
+++ Undesirable/Assets/PickUpProps.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickUpProps : MonoBehaviour
+{
+
+    public Image image;
+    public Sprite sp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //碰撞检测
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Prop")
+        {
+            Destroy(collider.gameObject);
+            image.sprite = sp;
+        }
+    }
+}
