afc4048252e4bd82cde2d024b7d895a3e443c5d0
570189458 <570189458@qq.com>
2019-07-06 08:54:38 +0800

    new

---
+++ Undesirable/Assets/Resouse/Scripts/Player01.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player01 : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(this.transform.position.y<-10)
+        {
+            //复位
+        }
+
+        Player01Control();
+    }
+
+    public void Player01Control()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        this.transform.Translate(new Vector3(h, 0, v) * speed * Time.deltaTime);
+    }
+}
