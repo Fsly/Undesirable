e2b716dadd0f5d691a54c9a968dfd92c8d71d9d5
570189458 <570189458@qq.com>
2019-07-06 12:16:59 +0800

    1

---
+++ Assets/PlaneControl.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneControl : MonoBehaviour
+{
+    public int DestoryMinNum;
+    public int DestoryMaxNum;
+    public List<GameObject> planeList;
+    public List<int> changeList;
+
+    public Material nonalMat;
+    public Material warningMat;
+
+    public float startTime;
+    public float destoryTime;
+    public float waitTime;
+
+    public bool isStartTime = true;
+    public bool isWaitTime;
+    public bool isDestoryTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartTimeDoSomething();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if(isStartTime)
+        {
+            startTime -= Time.deltaTime;
+            if(startTime<=0)
+            {
+                isStartTime = false;
+                isWaitTime = true;
+                waitTime = 2f;
+                //变色
+                ChangeColor();
+            }
+        }
+        if(isWaitTime)
+        {
+            waitTime -= Time.deltaTime;
+            if(waitTime<=0)
+            {
+                isWaitTime = false;
+                isDestoryTime = true;
+                destoryTime = 5f;
+                //破坏
+                DestoryPlane();
+            }
+        }
+        if(isDestoryTime)
+        {
+            destoryTime -= Time.deltaTime;
+            if(destoryTime<=0)
+            {
+                isDestoryTime = false;
+                isStartTime = true;
+                startTime = 5f;
+                //恢复
+                ResetPlane();
+                ResetPlaneColor();
+                StartTimeDoSomething();
+            }
+        }
+
+    }
+
+    public void StartTimeDoSomething()
+    {
+        //随机位置
+        changeList.Clear();
+        int destoryNum = Random.Range(DestoryMinNum, DestoryMaxNum);
+        Debug.Log(destoryNum);
+        for (int i = 0; i < destoryNum; i++)
+        {
+            int index = Random.Range(0, planeList.Count - 1);
+            while (changeList.Exists(s => s.Equals(index)))
+            {
+                //存在
+                index = Random.Range(0, planeList.Count - 1);
+            }
+            changeList.Add(index);
+        }
+    }
+
+    public void ChangeColor()
+    {
+        for (int i = 0; i < changeList.Count; i++)
+        {
+            planeList[changeList[i]].GetComponent<MeshRenderer>().materials = new Material[2] { nonalMat, warningMat };
+        }
+    }
+
+    public void DestoryPlane()
+    {
+        for (int i = 0; i < changeList.Count; i++)
+        {
+            planeList[changeList[i]].SetActive(false);
+        }
+    }
+
+    public void ResetPlane()
+    {
+        for (int i = 0; i < changeList.Count; i++)
+        {
+            planeList[changeList[i]].SetActive(true);
+        }
+    }
+
+    public void ResetPlaneColor()
+    {
+        for (int i = 0; i < changeList.Count; i++)
+        {
+            planeList[changeList[i]].GetComponent<MeshRenderer>().materials = new Material[2] { nonalMat, nonalMat };
+        }
+    }
+}
---
+++ Assets/Resouse/Scripts/Player02.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player02 : MonoBehaviour
+{
+    private Animator anim;
+
+    public float speed;
+
+    private int State;
+    private int oldState;
+    private int up = 0;
+    private int right = 1;
+    private int down = 2;
+    private int left = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position.y < -10)
+        {
+            //复位
+        }
+
+        Player02Control();
+    }
+
+    public void Player02Control()
+    {
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            SetState(up);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            SetState(down);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            SetState(left);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            SetState(right);
+        }
+        else
+        {
+            anim.SetFloat("Run", 0);
+        }
+    }
+
+    public void SetState(int currState)
+    {
+        Vector3 transformValue = new Vector3();
+        int rotateValue = (currState - State) * 90;
+        //动画
+        anim.SetFloat("Run", 1);
+        switch (currState)
+        {
+            case 0://角色状态向前时，角色不断向前缓慢移动
+                transformValue = Vector3.forward * Time.deltaTime * speed;
+                break;
+            case 1://角色状态向右时。角色不断向右缓慢移动
+                transformValue = Vector3.right * Time.deltaTime * speed;
+                break;
+            case 2://角色状态向后时。角色不断向后缓慢移动
+                transformValue = Vector3.back * Time.deltaTime * speed;
+                break;
+            case 3://角色状态向左时，角色不断向左缓慢移动
+                transformValue = Vector3.left * Time.deltaTime * speed;
+                break;
+        }
+        transform.Rotate(Vector3.up, rotateValue);
+        transform.Translate(transformValue, Space.World);
+        oldState = State;
+        State = currState;
+    }
+}
